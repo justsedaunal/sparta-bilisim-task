b73472012a6bec1a21c017dd560913d744341937
Seda Unal <sedaylmz06@outlook.com>
2023-05-11 20:51:41 +0300

    first commit

---
+++ pages/api/client-side.js
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+const ClientSide = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [reload, setReload] = useState()
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://randomfox.ca/floof/')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [reload]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
+  return (
+    <div>
+      <img src={data.image} />
+      <button onClick={() => setReload(!reload)} >Reload</button>
+    </div>
+  );
+};
+
+export default ClientSide;
