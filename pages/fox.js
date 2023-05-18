668502b51be380e3780e8f0305869cdf881f1c32
Seda Unal <sedaylmz06@outlook.com>
2023-05-18 03:19:02 +0300

    spinners editted

--- pages/fox.js
+++ pages/fox.js
@@ -17,45 +17,53 @@ const Fox = () => {
       });
   }, [reload]);
 
-  if (isLoading)
-    return (
-    
-        <Spin tip="Loading" size="large">
-          <div className="content articles__article" />
-        </Spin>
-
-    );
-  if (!data) return <p class="articles__article">No data</p>;
-
   return (
     <>
+      {!data ? (
+        <Spin
+          tip="Loading"
+          size="large"
+          style={{ maxHeight: "150px", minHeight: "150px" }}
+        ></Spin>
+      ) : (
+        <>
+          {isLoading ? (
+            <Spin
+              tip="Loading"
+              size="large"
+              style={{ maxHeight: "150px", minHeight: "150px" }}
+            ></Spin>
+          ) : (
+            <li
+              class="articles__article"
+              style={{ "--animation-order": 3 }}
+              onClick={() => setReload(!reload)}
+            >
+              <a class="articles__link">
+                <div class="articles__content articles__content--lhs">
+                  <img src={data.image} class="articles__title" />
+                  <div class="articles__footer">
+                    {/* <p>Puddings</p> */}
+                    {/* <time>3 Mar 2020</time> */}
+                  </div>
+                </div>
+                <div
+                  class="articles__content articles__content--rhs"
+                  aria-hidden="true"
+                >
+                  <img src={data.image} class="articles__title" />{" "}
+                  <div class="articles__footer">
+                    {/* <p>Puddings</p> */}
+                    {/* <time>3 Mar 2020</time> */}
+                  </div>
+                </div>
+              </a>
+              <button class="button">next</button>
+            </li>
+          )}
+        </>
+      )}
       {/* <button onClick={() => setReload(!reload)} >Reload</button> */}
-      <li
-        class="articles__article"
-        style={{ "--animation-order": 3 }}
-        onClick={() => setReload(!reload)}
-      >
-        <a class="articles__link">
-          <div class="articles__content articles__content--lhs">
-            <img src={data.image} class="articles__title" />
-            <div class="articles__footer">
-              {/* <p>Puddings</p> */}
-              {/* <time>3 Mar 2020</time> */}
-            </div>
-          </div>
-          <div
-            class="articles__content articles__content--rhs"
-            aria-hidden="true"
-          >
-            <img src={data.image} class="articles__title" />{" "}
-            <div class="articles__footer">
-              {/* <p>Puddings</p> */}
-              {/* <time>3 Mar 2020</time> */}
-            </div>
-          </div>
-        </a>
-        <button class="button">next</button>
-      </li>
     </>
   );
 };
--- pages/stoa.js
+++ pages/stoa.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { Spin } from "antd";
-import { Empty } from 'antd';
+import { Empty } from "antd";
 
 const Stoa = () => {
   const [data, setData] = useState(null);
@@ -18,35 +18,43 @@ const Stoa = () => {
       });
   }, [reload]);
 
-
-  if (isLoading) return  <Spin tip="Loading" size="large">
-  <div className="content articles__article" />
-</Spin>;
-  if (!data) return <Empty />;
-
   return (
-    <li class="articles__article" style={{ "--animation-order:": 2 }} onClick={() => setReload(!reload)} >
-      <a class="articles__link">
-        <div class="articles__content articles__content--lhs">
-          <h2 class="articles__title"> {data.quote} </h2>
-          <div class="articles__footer">
-            <p>{data.author} </p>
-            {/* <time>2 Feb 2020</time> */}
-          </div>
-        </div>
-        <div
-          class="articles__content articles__content--rhs"
-          aria-hidden="true"
+    <>
+      {!data ? (
+        <Spin
+          tip="Loading"
+          size="large"
+          style={{ maxHeight: "150px", minHeight: "150px" }}
+        ></Spin>
+      ) : (
+        <li
+          class="articles__article"
+          style={{ "--animation-order:": 2 }}
+          onClick={() => setReload(!reload)}
         >
-          <h2 class="articles__title"> {data.quote} </h2>
-          <div class="articles__footer">
-            <p>Chocolates</p>
-            {/* <time>2 Feb 2020</time> */}
-          </div>
-        </div>
-      </a>
-      <button class="button"  >next</button>
-    </li>
+          <a class="articles__link">
+            <div class="articles__content articles__content--lhs">
+              <h2 class="articles__title"> {data.quote} </h2>
+              <div class="articles__footer">
+                <p>{data.author} </p>
+                {/* <time>2 Feb 2020</time> */}
+              </div>
+            </div>
+            <div
+              class="articles__content articles__content--rhs"
+              aria-hidden="true"
+            >
+              <h2 class="articles__title"> {data.quote} </h2>
+              <div class="articles__footer">
+                <p>Chocolates</p>
+                {/* <time>2 Feb 2020</time> */}
+              </div>
+            </div>
+          </a>
+          <button class="button">next</button>
+        </li>
+      )}
+    </>
   );
 };
 
